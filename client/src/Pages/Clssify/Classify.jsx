a5ec5158235b127fa44667bda3e563aabd7a6f6a
mxnattha <mx.nattha963@gmail.com>
2024-07-23 23:04:24 +0700

    WIP: Started develope new feature [classification dashboard]

--- client/src/Pages/Clssify/Classify.jsx
+++ client/src/Pages/Clssify/Classify.jsx
@@ -11,6 +11,10 @@ function Classify() {
     const [imageUrl, setImageUrl] = useState(null);
     const [prediction, setPrediction] = useState(null);
     const [loading, setLoading] = useState(false);
+    const [model, setModel] = useState('');
+    const [dropdownOpen, setDropdownOpen] = useState(false);
+    const [placeHolder, setPlaceHolder] = useState('Select Model');
+    const [modelData, setModelData] = useState({ top1Accuracy: '-', top5Accuracy: '-', numParameters: '-' });
 
     const handleImageUpload = (event) => {
         console.log('this function triggerd')
@@ -66,53 +70,144 @@ function Classify() {
         handleDisplayClick();
     };
 
+    const handleModelSelect = (selectedModel) => {
+        setModel(selectedModel);
+        setPlaceHolder(selectedModel);
+        setModelData(models[selectedModel]);
+        setDropdownOpen(false);
+    }
+
+    const models = {
+        'Efficientnet B0': { top1Accuracy: '77.7%', top5Accuracy: '93.5%', numParameters: '5.3M' },
+        'Efficientnet B1': { top1Accuracy: '78.6%', top5Accuracy: '94.2%', numParameters: '7.8M' },
+        'Efficientnet B2': { top1Accuracy: '80.6%', top5Accuracy: '95.3%', numParameters: '9.1M' },
+        'Efficientnet B3': { top1Accuracy: '82.0%', top5Accuracy: '96.0%', numParameters: '12.2M' },
+        'Efficientnet B4': { top1Accuracy: '83.4%', top5Accuracy: '96.6%', numParameters: '19.3M' },
+        'Efficientnet B5': { top1Accuracy: '83.4%', top5Accuracy: '96.6%', numParameters: '30.4M' },
+        'Efficientnet B6': { top1Accuracy: '94.0%', top5Accuracy: '96.9%', numParameters: '43.0M' },
+        'Efficientnet B7': { top1Accuracy: '84.1%', top5Accuracy: '97.0%', numParameters: '66.3M' }
+    };
+
     return(<div>
                 <Nav />
-                <h2 className='model-header'>Classification Model</h2>
-                <div className='upload-container' onClick={handleDisplayClick}>
-                    {imageUrl ? null : (<img src={upload} alt="upload-logo" className='upload-logo'/>)}
-                    
-                    {imageUrl ? (<img src={imageUrl} alt="uploaded-img" className='uploaded-img'/>) : 
-                                (<p>Click to upload your image here</p>)}
+                <div className='model-header'>
+                    <h2>Classification Model</h2>
+                </div>
+                
+                <div className='classification-features'>
+
+
+                    {/* section 1 */}
+                    <div className='dashboard-body'>
+                        <div className='control-panel'>
+
+                            <div className='model-dropdown-box'>
+                                
+                                <input 
+                                    type="text" 
+                                    className='textBox' 
+                                    placeholder={placeHolder}
+                                    readonly
+                                    onClick={() => setDropdownOpen(!dropdownOpen)}
+                                />
+
+                                
+                                {dropdownOpen && (
+                                <div className='model-option'>
+                                    {Object.keys(models).map((modelKey) => (
+                                        <div key={modelKey} onClick={() => handleModelSelect(modelKey)}>
+                                            {modelKey}
+                                        </div>
+                                    ))}
+                                </div>
+                            )}
+                            </div>
+                            {!image && <button className='upload-button' onClick={handleDisplayClick}>Upload new image</button>}
+                            {image && <button className='upload-button' onClick={handleNewImageUpload}>Upload new image</button>}
+                            {image && <button className='predict-button' onClick={handlePrediction}>Let's Predict</button>}
+                            
+                        </div>
+
+                        <div className='dashboard-box'>
+                        <div className='dashboard-container'>
+                            {Object.entries(modelData).map(([key, value], index) => (
+                                <div className='dashboard-card' key={index}>
+                                    <div className='dashboard-value'>{value}</div>
+                                    <div className='dashboard-title'>{key.replace(/([A-Z])/g, ' $1')}</div>
+                                </div>
+                            ))}
+                        </div>
+                        </div>
+                    </div>
+
+
+                    {/* section 2 */}
+                    <div className='prediction-body'>
+
+                        <div className='upload-box'>
+                            <div className='upload-container'>
+                                {imageUrl ? null : (<img src={upload} alt="upload-logo" className='upload-logo'/>)}
+                                
+                                {imageUrl ? (<img src={imageUrl} alt="uploaded-img" className='uploaded-img'/>) : 
+                                            (<p> upload your image</p>)}
+                            </div>
+
+                            <input 
+                                type="file" 
+                                id='fileInput'
+                                style={{display: 'none'}}
+                                onChange = {handleImageUpload}
+                            />
+                        
+                
+                        </div>
+
+                        {/* <div className='buttons-box'>
+                            <div className='buttons'>
+                                {!image && <button className='upload-button' onClick={handleDisplayClick}>Upload new image</button>}
+                                {image && <button className='upload-button' onClick={handleNewImageUpload}>Upload new image</button>}
+                                {image && <button className='predict-button' onClick={handlePrediction}>Let's Predict</button>}
+                            </div>
+                        </div> */}
+
+
+
+                        <div className='prediction-box'>
+                            
+                                {loading && (
+                                <div className='loader-container'>
+                                    <Oval
+                                        height={80}
+                                        width={80}
+                                        color="#5A639C"
+                                        wrapperStyle={{}}
+                                        wrapperClass=""
+                                        visible={true}
+                                        ariaLabel='oval-loading'
+                                        secondaryColor="#9B86BD"
+                                        strokeWidth={2}
+                                        strokeWidthSecondary={2}
+                                    />
+                                </div>
+                                )}
+                                {prediction && (
+                                    <div>
+                                        <ProgressBar label1={prediction.Class1} 
+                                                    value1={prediction.Prob1}
+                                                    label2={prediction.Class2}
+                                                    value2={prediction.Prob2}
+                                                    label3={prediction.Class3}
+                                                    value3={prediction.Prob3}
+                                                    label4={prediction.Class4}
+                                                    value4={prediction.Prob4}
+                                        />
+                                    </div>
+                                )}
+                            
+                        </div>
+                    </div>
                 </div>
 
-                <input 
-                    type="file" 
-                    id='fileInput'
-                    style={{display: 'none'}}
-                    onChange = {handleImageUpload}
-                />
-                {image && <button className='predict-button' onClick={handlePrediction}>Let's Predict</button>}
-                {image && <button className='upload-button' onClick={handleNewImageUpload}>Upload new image</button>}
-                {loading && (
-                <div className='loader-container'>
-                    <Oval
-                        height={80}
-                        width={80}
-                        color="#5A639C"
-                        wrapperStyle={{}}
-                        wrapperClass=""
-                        visible={true}
-                        ariaLabel='oval-loading'
-                        secondaryColor="#9B86BD"
-                        strokeWidth={2}
-                        strokeWidthSecondary={2}
-                    />
-                </div>
-                )}
-                {prediction && (
-                    <div>
-                        <ProgressBar label1={prediction.Class1} 
-                                     value1={prediction.Prob1}
-                                     label2={prediction.Class2}
-                                     value2={prediction.Prob2}
-                                     label3={prediction.Class3}
-                                     value3={prediction.Prob3}
-                                     label4={prediction.Class4}
-                                     value4={prediction.Prob4}
-                        />
-                    </div>
-                )}
             </div>);
 }
 
